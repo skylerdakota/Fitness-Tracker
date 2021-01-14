a180c429383f7dca798a16be525f7cf1a8acd570
Skyler Dakota Lawton <skylerdakota@gmail.com>
2021-01-14 11:06:24 -0500

    cleaned up file from commented out coding trials

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -23,31 +23,3 @@ router.get("/stats", (req, res) =>{
 });
 
 module.exports = router;
-
-
-// // Require in npm packages
-// const path = require("path");
-
-// module.exports = function(app){
-
-//     // Send user to homepage
-//     app.get("/", (req, res) =>{
-//         res.sendFile(path.join(__dirname, "../public/index.html"))
-//     });
-
-//     // Send user to a new exercise web page
-//     app.get("/exercise", (req, res) =>{
-//         res.sendFile(path.join(__dirname, "../public/exercise.html"))
-//     });
-
-//     // Send user to an old exercise web page
-//     // app.get("/exercise?", (req, res) =>{
-//     //     res.sendFile(path.join(__dirname, "../public/exercise?.html"))
-//     // });
-
-//     // Send user to stats web page
-//     app.get("/stats", (req, res) =>{
-//         res.sendFile(path.join(__dirname, "../public/stats.html"))
-//     });
-
-// }
